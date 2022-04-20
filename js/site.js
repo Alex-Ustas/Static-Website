f1ef59fc780e1d7cc2e81480cc766fdbcb8da4a6
Alex Ustas <alex.ustas@internet.ru>
2022-04-20 08:40:38 +0300

    ver1.01

--- js/site.js
+++ js/site.js
@@ -1,6 +1,4 @@
 ﻿let d = new Date();
 /*alert('*** My Site ***');*/
 document.body.innerHTML = "<h1 style='color:black'>Сегодня " + d + "</h1>";
-document.body.innerHTML = "<h1 font='Times New Roman'>Сегодня " + d + "</h1>";
-document.body.innerHTML = "<h1 font='Arial'>Сегодня " + d + "</h1>";
-document.body.innerHTML = "<h1 font='Comic Sans MS'>Сегодня " + d + "</h1>";
+
